e2b2233f63747a132981c9b69e1f179f273de6d0
CincaVictor <svictor.cinca@itis.pr.it>
2024-01-12 13:05:40 +0100

    secondo upload

---
+++ Main.js
@@ -0,0 +1,6 @@
+//start 
+
+//Un convertitore di valuta permette di calcolare il valore di un importo di denaro
+//espresso in una valuta (ad esempio in Euro) 
+//nel corrispondente importo espresso in un’altra valuta (ad esempio dollaro)
+
